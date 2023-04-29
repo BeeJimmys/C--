bf9be3560ef64ccefe6b96bee1a233e826b1cbc8
BeeJimmys <zkq20031217@foxmail.com>
2023-04-29 16:08:18 +0800

    003

---
+++ workManager.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class WorkManager
+{
+public:
+    WorkManager();
+
+    void showMenu();
+
+    ~WorkManager();
+};
+
+WorkManager::WorkManager()
+{
+}
+void WorkManager::showMenu()
+{
+    cout << "------------------------------" << endl;
+    cout << "      欢迎使用学生管理系统      " << endl;
+    cout << "           0.退出系统          " << endl;
+    cout << "           1.添加信息          " << endl;
+    cout << "           2.显示信息          " << endl;
+    cout << "           3.删除信息          " << endl;
+    cout << "           4.修改信息          " << endl;
+    cout << "           5.查找信息          " << endl;
+    cout << "           6.信息排序          " << endl;
+    cout << "           7.清空信息          " << endl;
+    cout << endl;
+}
+WorkManager::~WorkManager()
+{
+}
---
+++ 管理系统.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#include "workManager.h"
+int main()
+{
+    WorkManager wm;
+
+    string choice = "0";
+    wm.showMenu();
+    system("pause");
+    return 0;
+}
